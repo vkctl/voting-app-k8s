78552690ee4ef4333af1acc9c96733808691ad0a
varun <goudvarunkumar@gmail.com>
2026-09-15 17:14:27 +0530

    feat: add kubernetes provider

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -5,6 +5,10 @@ terraform {
             source = "hashicorp/aws"
             version = "~> 5.0"
         }
+        kubernetes = {
+            source = "hashicorp/kubernetes"
+            version = "~> 2.31"
+        }
     }
 }
 
