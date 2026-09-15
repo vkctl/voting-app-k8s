8e3689f41db36f64a87b2a125c7e51dc158ced82
varun <goudvarunkumar@gmail.com>
2026-09-15 12:15:36 +0530

    feat: add aws network terraform config

---
+++ infra/terraform/network.tf
@@ -0,0 +1,52 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "${var.project_name}-vpc"
+    }
+}
+
+resource "aws_internet_gateway" "main"{
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "${var.project_name}-igw"
+    }
+}
+
+data "aws_available_zones" "available" {
+    state = "available"
+}
+
+resource "aws_subnet" "public" {
+    count = 2
+    vpc_id = aws_vpc.main.id
+    cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+    availability_zone = data.aws_available_zones.available.names[count.index]
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "${var.project_name}-public-${count.index}"
+        "kubernetes.io/clusters/${var.cluster_name}" = "shared"
+        "kubernetes.io/role/elb" = "1"
+    }
+}
+
+resource "aws_route_table" "public" {
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+        Name = "${var.project_name}-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "public" {
+    count = 2
+    subnet_id = aws_subnet.public[count.index].id
+    route_table_id = aws_route_table.public.id
+}
