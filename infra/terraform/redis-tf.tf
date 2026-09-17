053b917cb69e6a8ccf626363b9726508554a8e8e
varun <goudvarunkumar@gmail.com>
2026-09-17 10:25:41 +0530

    feat: add kube redis terraform config

---
+++ infra/terraform/redis-tf.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_deployment_v1" "redis" {
+    metadata {
+        name = "redis"
+    }
+
+    spec {
+        replicas = 1
+
+        selector {
+            match_labels = {
+                app = "redis"
+            }
+        }
+
+        template {
+            metadata {
+                labels = {
+                    app = "redis
+                }
+            }
+
+            spec {
+                container {
+                    name = "redis"
+                    image = "redis:alpine"
+
+                    port{
+                        container_port = 6379
+                    }
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service_v1" "redis" {
+    metadata {
+        name = "redis"
+    }
+
+    spec {
+        selector = {
+            app = "redis"
+        }
+
+        port{
+            port = 6379
+            target_port = 6379
+        }
+    }
+}
