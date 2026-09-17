ec77f6600e430f1f8841b68cc4e9daa866b8abf9
varun <goudvarunkumar@gmail.com>
2026-09-17 17:31:30 +0530

    feat: add terraform provider with kubernetes

---
+++ infra/terraform/app/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">= 1.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.31"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-2"   # must match whatever region cluster/ actually created the EKS cluster in
+}
+
+data "aws_eks_cluster" "main" {
+  name = "voting-app"
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = "voting-app"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
