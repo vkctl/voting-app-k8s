6541dd93ba55e6175246ecf9264b438fd848d583
varun <goudvarunkumar@gmail.com>
2026-09-17 17:12:12 +0530

    chore: remove kubernetes config from cluster creation

--- infra/terraform/cluster/providers.tf
+++ infra/terraform/cluster/providers.tf
@@ -5,10 +5,6 @@ terraform {
             source = "hashicorp/aws"
             version = "~> 5.0"
         }
-        kubernetes = {
-            source = "hashicorp/kubernetes"
-            version = "~> 2.31"
-        }
     }
 }
 
