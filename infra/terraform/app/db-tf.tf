170a778f5040b39dfb88daff9aedc090025d2fc0
varun <goudvarunkumar@gmail.com>
2026-09-18 14:50:10 +0530

    feat: add postgres kube terraform config

---
+++ infra/terraform/app/db-tf.tf
@@ -0,0 +1,98 @@
+resource "kubernetes_secret_v1" "db_credentials" {
+  metadata {
+    name = "db-credentials"
+  }
+
+  data = {
+    POSTGRES_USER     = "postgres"
+    POSTGRES_PASSWORD = "postgres"
+    POSTGRES_DB       = "postgres"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim_v1" "db_data" {
+  metadata {
+    name = "db-data"
+  }
+
+  spec {
+    access_modes       = ["ReadWriteOnce"]
+    storage_class_name = kubernetes_storage_class_v1.ebs_gp3.metadata[0].name
+
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment_v1" "db" {
+  metadata {
+    name = "db"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "db"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "db"
+        }
+      }
+
+      spec {
+        container {
+          name  = "db"
+          image = "postgres:15-alpine"
+
+          port {
+            container_port = 5432
+          }
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret_v1.db_credentials.metadata[0].name
+            }
+          }
+
+          volume_mount {
+            name       = "db-storage"
+            mount_path = "/var/lib/postgresql/data"
+          }
+        }
+
+        volume {
+          name = "db-storage"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim_v1.db_data.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "db" {
+  metadata {
+    name = "db"
+  }
+
+  spec {
+    selector = {
+      app = "db"
+    }
+
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+  }
+}
