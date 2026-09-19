5b7303c9b78315a42bcbe90e0f39bde3721a4eb1
varun <goudvarunkumar@gmail.com>
2026-09-19 13:26:32 +0530

    feat: add vote kube deploy & service terraform config

---
+++ infra/terraform/app/vote-tf.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_deployment_v1" "vote" {
+  metadata {
+    name = "vote"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "vote"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "vote"
+        }
+      }
+
+      spec {
+        container {
+          name  = "vote"
+          image = "ghcr.io/${var.image_owner}/vote:${var.vote_image_tag}"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "vote" {
+  metadata {
+    name = "vote"
+  }
+
+  spec {
+    selector = {
+      app = "vote"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
