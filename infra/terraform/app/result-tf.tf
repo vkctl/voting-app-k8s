c1881a0ef3e92ae896bdf969ed1f59929736d9f3
varun <goudvarunkumar@gmail.com>
2026-09-19 13:43:44 +0530

    feat: add result kube deploy & service terraform config

---
+++ infra/terraform/app/result-tf.tf
@@ -0,0 +1,51 @@
+resource "kubernetes_deployment_v1" "result" {
+  metadata {
+    name = "result"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "result"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "result"
+        }
+      }
+
+      spec {
+        container {
+          name  = "result"
+          image = "ghcr.io/${var.image_owner}/result:${var.result_image_tag}"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "result" {
+  metadata {
+    name = "result"
+  }
+
+  spec {
+    selector = {
+      app = "result"
+    }
+
+    port {
+      port = 80
+      target_port = 80
+    }
+  }
+}
