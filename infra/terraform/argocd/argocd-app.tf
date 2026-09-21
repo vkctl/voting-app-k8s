4731f404b87e27d23b9435d95261e7f3aa33ba15
varun <goudvarunkumar@gmail.com>
2026-09-21 10:56:54 +0530

    feat: point argocd to repo & new vote config

---
+++ infra/terraform/argocd/argocd-app.tf
@@ -0,0 +1,33 @@
+resource "kubernetes_manifest" "vote_app" {
+    manifest = {
+        apiVersion = "argoproj.io/v1alpha1"
+        kind = "Application"
+        metadata = {
+            name = "vote"
+            namespace = "argocd"
+        }
+        spec = {
+            project = "default"
+
+            source = {
+                repoURL = "https://github.com/vkctl/voting-app-k8s.git"
+                targetRevision = "main"
+                path = "k8s/argocd"
+            }
+
+            destination = {
+                server = "https://kubernetes.default.svc"
+                namespace = "default"
+            }
+
+            syncPolicy = {
+                automated = {
+                    prune = true
+                    selfHeal = true
+                }
+            }
+        }
+    }
+
+    depends_on = [helm_release.argocd]
+}
