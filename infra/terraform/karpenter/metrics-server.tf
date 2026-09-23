9dadb11b306bbb9edf7d83b41a73a40dd2fdaeba
varun <goudvarunkumar@gmail.com>
2026-09-23 17:09:56 +0530

    feat: add metrics server

---
+++ infra/terraform/karpenter/metrics-server.tf
@@ -0,0 +1,6 @@
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+}
