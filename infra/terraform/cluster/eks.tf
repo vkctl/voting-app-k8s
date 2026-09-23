b87227f96c22c79c8b0cec93ee3d3d9d1ba89a77
varun <goudvarunkumar@gmail.com>
2026-09-23 14:58:12 +0530

    chore: bump desired & max node count

--- infra/terraform/cluster/eks.tf
+++ infra/terraform/cluster/eks.tf
@@ -9,18 +9,21 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
  
-#  enable_irsa = true
 
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
 
+# Currently t3.small has max cap of pods at 11 due to AWS_CNI
+# Alternates option a: include Prefix delegation to vpc cni addon - assigns 110 ips to a node so it, to limit add bootstrap args to eks managed node eks_managed_node_groups
+# option b: use cilium
+# option c: increase desired nodes
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
+      max_size       = 5
+      desired_size   = 3
     }
   }
 
